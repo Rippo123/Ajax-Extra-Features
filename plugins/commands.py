29428208d5e2a687f4c3b212a0d37e7cd9ff28c3
Rippo123 <98217663+Rippo123@users.noreply.github.com>
2022-03-24 17:20:06 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -97,11 +97,9 @@ async def start(client, message):
             InlineKeyboardButton('🔎 SEᗩᖇᑕᕼ ᕼEᖇE 🔍', switch_inline_query_current_chat='')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_photo(
-            photo=random.choice(PICS),
-            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
-            reply_markup=reply_markup,
-            parse_mode='html'
+        await message.reply_chat_action("typing")
+ m=await message.reply_sticker("CAACAgUAAxkBAAEO6RtiO7D4w8Paf") 
+await asyncio.sleep(1) await m.delete()
          )
         return
     data = message.command[1]
