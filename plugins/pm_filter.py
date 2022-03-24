e8d3bf90ab27830fc052f1834ae4f4f2d20852e9
Rippo123 <98217663+Rippo123@users.noreply.github.com>
2022-03-24 19:39:00 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -738,6 +738,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             text="▣▣▣"
         )  
         reply_markup = InlineKeyboardMarkup(buttons)
+        message.reply_chat_action("typing")
         await query.message.edit_text(
             text=script.START_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
@@ -832,6 +833,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             text="▣▣▣"
         )
         reply_markup = InlineKeyboardMarkup(buttons)
+        message.reply_chat_action("typing")
         await query.message.edit_text(
             text=script.HELP_TXT.format(query.from_user.mention),
             reply_markup=reply_markup,
@@ -887,6 +889,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             text="▣▣▣"
         )
         reply_markup = InlineKeyboardMarkup(buttons)
+        message.reply_chat_action("typing")
         await query.answer("𝖶𝖾𝗅𝖼𝗈𝗆𝖾 𝗍𝗈 𝗆𝗒 𝖧𝖾𝗅𝗉 𝗆𝗈𝖽𝗎𝗅𝖾")
         await query.message.edit_text(
             text=script.HELP_TXT.format(query.from_user.mention),
@@ -910,6 +913,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             text="▣▣▣"
         )
         reply_markup = InlineKeyboardMarkup(buttons)
+        message.reply_chat_action("typing")
         await query.message.edit_text(
             text=script.ABOUT_TXT.format(temp.B_NAME),
             reply_markup=reply_markup,
