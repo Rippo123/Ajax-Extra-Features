1db3f9a7896324c83cc345ec231209cd360a1f8a
Rippo123 <98217663+Rippo123@users.noreply.github.com>
2022-03-24 17:01:15 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -53,10 +53,12 @@ async def start(client, message):
             InlineKeyboardButton('🔎 SEᗩᖇᑕᕼ ᕼEᖇE 🔍', switch_inline_query_current_chat='')
         ]]  
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_chat_action("typing")
- m=await message.reply_sticker("sd4NCdvg8efiU1wACFQEAAsiUZBRmRDCipxVsEyME") 
-await asyncio.sleep(1) await m.delete()
-
+        message.reply_chat_action("typing")
+        await message.reply_photo(
+            photo=random.choice(PICS),
+            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+            reply_markup=reply_markup,
+            parse_mode='html'
        )
        return
     if AUTH_CHANNEL and not await is_subscribed(client, message):
